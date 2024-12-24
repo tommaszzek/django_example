589546d880fda8ffff5bb13342c2c49af7c7efc2
Tomasz <tommaszzek@gmail.com>
2024-12-24 07:14:58 +0100

    renderowanie z pliku html

--- app_1/views.py
+++ app_1/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
 
 # Create your views here.
-def index(req):
-    return HttpResponse('dosik')
+def index(request):
+    # return HttpResponse('dosik')
+    return render(request,'app_1/dane.html')
     
